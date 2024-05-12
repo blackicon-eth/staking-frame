2913b6373843d60bf94a39aa9d27994022238667
Mattia Verdecchi <mattiaverdecchi@yahoo.it>
2024-05-12 17:00:47 +0200

    Added a support function to get the first frame

--- app/lib/getFrame.ts
+++ app/lib/getFrame.ts
@@ -33,3 +33,31 @@ export function getUpdateFrame(): NextResponse {
 
   return new NextResponse(frame);
 }
+
+export function getFirstFrame(): NextResponse {
+  const frame = getFrameHtmlResponse({
+    input: {
+      text: "Ask a question or insert amount",
+    },
+    buttons: [
+      {
+        label: "Ask Brian 🧠",
+        action: "post",
+        target: `${process.env.NEXT_PUBLIC_BASE_URL}/api/main?action=ask?state=start`,
+      },
+      {
+        label: "Stake ETH",
+        action: "post",
+        target: `${process.env.NEXT_PUBLIC_BASE_URL}/api/main?action=deposit?state=start`,
+      },
+      {
+        label: "Withdraw ETH",
+        action: "post",
+        target: `${process.env.NEXT_PUBLIC_BASE_URL}/api/main?action=withdraw?state=start`,
+      },
+    ],
+    image: { src: `${process.env.NEXT_PUBLIC_BASE_URL}/frames/1to1.png`, aspectRatio: "1:1" },
+  });
+
+  return new NextResponse(frame);
+}
