0fe8f1a147ee645b23e79d34f1f39feeff6c3f9a
Mattia Verdecchi <mattiaverdecchi@yahoo.it>
2024-05-16 23:30:34 +0200

    wip: removed unused code

--- app/lib/generateImage.jsx
+++ app/lib/generateImage.jsx
@@ -3,53 +3,12 @@ import { readFileSync } from "fs";
 import { join } from "path";
 import * as style from "./style_components/styles";
 import sharp from "sharp";
-import axios from "axios";
 
 // Load the font
 const font = join(process.cwd(), "public/fonts/GothamBoldItalic.ttf");
 const GothamBoldItalicBuffer = readFileSync(font);
 
-// Support function to take the first frame of a GIF
-async function checkAndGetPicture(pictureUrl, username) {
-  if (pictureUrl && username) {
-    try {
-      // Get the image as an ArrayBuffer
-      var response = await axios.get(pictureUrl, {
-        responseType: "arraybuffer",
-      });
-    } catch {
-      return { buffer: `${process.env.NEXT_PUBLIC_BASE_URL}/frames/not_found.png`, found: false };
-    }
-    // Convert the image to PNG and return it as an ArrayBuffer
-    const pngBuffer = await sharp(Buffer.from(response.data)).toFormat("png").toBuffer();
-    const pngArrayBuffer = pngBuffer.buffer.slice(pngBuffer.byteOffset, pngBuffer.byteOffset + pngBuffer.byteLength);
-
-    return { buffer: pngArrayBuffer, found: true };
-  }
-  return { buffer: `${process.env.NEXT_PUBLIC_BASE_URL}/frames/not_found.png`, found: false };
-}
-
-// Support function to get the data to compose the image
-async function getImageData(_callerUsername, _callerPropic, _friendUsername, _friendPropic) {
-  // Get the data to compose the image
-  const callerUsername = !_callerUsername || !_callerPropic ? "Not found..." : _callerUsername;
-  const friendUsername = !_friendUsername || !_friendPropic ? "Not found..." : _friendUsername;
-  const callerPropic = await checkAndGetPicture(_callerPropic, _callerUsername);
-  const friendPropic = await checkAndGetPicture(_friendPropic, _friendUsername);
-
-  // Get current date to show on the image
-  const formattedDate = new Date().toLocaleDateString();
-
-  // Get the background image
-  const bgImage =
-    !_callerUsername || !_friendUsername || !callerPropic.found || !friendPropic.found
-      ? `${process.env.NEXT_PUBLIC_BASE_URL}/frames/missing.png`
-      : `${process.env.NEXT_PUBLIC_BASE_URL}/frames/best_friends.png`;
-
-  return { formattedDate, bgImage, callerUsername, friendUsername, callerPropic, friendPropic };
-}
-
-export async function generateFriendImage(_callerUsername, _callerPropic, _friendUsername, _friendPropic) {
+export async function generateImage(_callerUsername, _callerPropic, _friendUsername, _friendPropic) {
   // Getting the data in order to compose the image
   const { formattedDate, bgImage, callerUsername, friendUsername, callerPropic, friendPropic } = await getImageData(
     _callerUsername,
