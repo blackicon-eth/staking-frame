3056d8c6915f2eddc9c53ef9c9ef882bfb3bae7b
Mattia Verdecchi <mattiaverdecchi@yahoo.it>
2024-05-17 01:36:26 +0200

    created correct styles

--- app/lib/style_components/styles.ts
+++ app/lib/style_components/styles.ts
@@ -8,44 +8,18 @@ export const bgImage = {
 
 export const background = {
   display: "flex",
-  flexDirection: "column",
-  gap: "-30",
   height: "100%",
   width: "100%",
   alignItems: "center",
   justifyContent: "center",
-  fontSize: 60,
-  letterSpacing: -2,
+  fontSize: 55,
+  letterSpacing: -1,
   fontWeight: 700,
 };
 
-export const callerContainer = {
+export const textContainer = {
   display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-  justifyContent: "center",
-  position: "absolute",
-  left: "30",
-  bottom: "230",
+  marginRight: "200px",
+  marginLeft: "230",
+  maxWidth: "1450",
 };
-
-export const friendContainer = {
-  display: "flex",
-  flexDirection: "column",
-  alignItems: "center",
-  justifyContent: "center",
-  position: "absolute",
-  left: "685",
-  bottom: "230",
-};
-
-export const imageFriend = {
-  height: "785",
-  width: "785",
-  borderRadius: "100%",
-};
-
-export const date = { position: "absolute", left: "13", bottom: "24" };
-
-export const twFriendName = "flex rounded-3xl bg-white p-15 pb-5 absolute -bottom-26";
-export const twDate = "flex justify-start items-start text-[35px] text-white";
