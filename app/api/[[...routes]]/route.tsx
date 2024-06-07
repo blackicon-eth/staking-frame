4f0e8ef500a331cbc67470b269e5179d95d8256e
Mattia Verdecchi <mattiaverdecchi@yahoo.it>
2024-06-08 00:51:41 +0200

    removed export functions

--- app/api/[[...routes]]/route.tsx
+++ app/api/[[...routes]]/route.tsx
@@ -38,7 +38,7 @@ const app = new Frog({
 // So here it is a "library" of functions that returns the frames
 
 // Returns an invalid frame
-export function getInvalidFrame(context: FrameContext<Env, "/", BlankInput>): TypedResponse<FrameResponse> {
+function getInvalidFrame(context: FrameContext<Env, "/", BlankInput>): TypedResponse<FrameResponse> {
   return context.res({
     image: `${process.env.NEXT_PUBLIC_BASE_URL}/frames/invalid.png`,
     intents: [<Button action="/">Start over</Button>],
@@ -46,7 +46,7 @@ export function getInvalidFrame(context: FrameContext<Env, "/", BlankInput>): Ty
 }
 
 // Returns an error frame
-export function getErrorFrame(context: FrameContext<Env, "/", BlankInput>): TypedResponse<FrameResponse> {
+function getErrorFrame(context: FrameContext<Env, "/", BlankInput>): TypedResponse<FrameResponse> {
   return context.res({
     image: `${process.env.NEXT_PUBLIC_BASE_URL}/frames/error.png`,
     intents: [<Button action="/">Start over</Button>],
@@ -54,7 +54,7 @@ export function getErrorFrame(context: FrameContext<Env, "/", BlankInput>): Type
 }
 
 // Returns a frame with a loading image
-export function getUpdateFrame(
+function getUpdateFrame(
   uuid: string,
   action: string,
   count: number,
@@ -71,10 +71,7 @@ export function getUpdateFrame(
 }
 
 // Returns a frame with the knowledge image
-export function getKnowledgeFrame(
-  redisResponse: any,
-  context: FrameContext<Env, "/", BlankInput>
-): TypedResponse<FrameResponse> {
+function getKnowledgeFrame(redisResponse: any, context: FrameContext<Env, "/", BlankInput>): TypedResponse<FrameResponse> {
   const text =
     "Lido Finance is a decentralized liquid staking solution that allows users to stake their assets securely and efficiently on various blockchain networks.\n\nLiquid staking refers to the process of staking assets in a network while also being able to use those staked assets as collateral or for other financial activities.\n\nThis differs from traditional staking, where assets are locked up and not readily accessible.\n\nLido Finance specifically focuses on liquid staking for Ethereum 2.0 and other proof-of-stake blockchains. Users can stake their ETH through Lido, receive stETH (liquid staked Ether) in return, and then use this stETH for further DeFi activities such as lending, borrowing, or trading. This enables users to earn staking rewards and participate in other financial activities, all while their ETH remains staked on the Ethereum 2.0 network.\n\nOne of the key benefits of using Lido Finance is that it allows users to participate in staking without having to meet the minimum requirements for staking themselves. By \n\npooling assets together through Lido, users can collectively stake and earn rewards, even with smaller amounts of assets.\n\nOverall, Lido Finance provides a convenient and accessible way for users to participate in liquid staking on Ethereum 2.0 and other proof-of-stake blockchains, opening up \n\nnew opportunities for DeFi innovation and participation.";
 
